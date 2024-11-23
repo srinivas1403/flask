bebe7f23e213d82a1afe5707863695e003a51608
Srinivas Raviganesh <64420460+srinivas1403@users.noreply.github.com>
2024-11-23 15:59:17 +0000

    comments updated

--- greetings.py
+++ greetings.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import request
 app = Flask(__name__)
-@app.route("/")#URL leading to method
-def hello(): # Name of the method
+@app.route("/")
+def hello():
  return("Hello World!")
-@app.route("/greetme")#different URL
-def helloall(): # different method name
- name = request.args.get('name')#retrieve GET parameters
- return("Hello {}!".format(name))#Python’s string.format
+@app.route("/greetme")
+def helloall():
+ name = request.args.get('name')
+ return("Hello {}!".format(name))
 if __name__ == "__main__":
- app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
+ app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem'))
